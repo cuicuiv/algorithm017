f5dd3787737edb60d311a064cfd88058575cde7b
cuicuiv <844410337@qq.com>
2020-11-15 21:59:40 +0800

    第七周

--- Week_07/单词接龙.java
+++ Week_07/单词接龙.java
@@ -1,67 +1,3 @@
->**字典数和并查集**
->字典树核心：空间换时间
->>典型应用是用于统计和排序大量的字符串，经常被搜索引擎系统用于文本词频统计
->>优点：最大限度地减少无谓的字符串比较，查询效率比哈希表搞
->>基本性质：①结点本身不存完整单词
->>②从根节点到某一结点，路径上经过的字符连接起来，为改结点对应的字符串
->>③每个结点的所有子节点路径代表的字符都不同
-
->还有其他的数据结构，如平衡树和哈希表，使我们能够在字符串数据集中搜索单词。为什么我们还需要 Trie 树呢？尽管哈希表可以在 O(1) 时间内寻找键值，却无法高效的完成以下操作：
->①找到具有同一前缀的全部键值。
->②按词典序枚举字符串的数据集。
->Trie 树优于哈希表的另一个理由是，随着哈希表大小增加，会出现大量的冲突，时间复杂度可能增加到 O(n)，其中 n是插入的键的数量。与哈希表相比，Trie 树在存储多个具有相同前缀的键时可以使用较少的空间。此时 Trie 树只需要 O(m)的时间复杂度，其中 m为键长。而在平衡树中查找键值需要 O(mlogn) 时间复杂度。
->。
-
-
-
-
-[有效的数独](https://leetcode-cn.com/problems/valid-sudoku/solution/you-xiao-de-shu-du-by-leetcode/)
-```java
-
-class Solution {
-  public boolean isValidSudoku(char[][] board) {
-    // init data
-    HashMap<Integer, Integer> [] rows = new HashMap[9];
-    HashMap<Integer, Integer> [] columns = new HashMap[9];
-    HashMap<Integer, Integer> [] boxes = new HashMap[9];
-    for (int i = 0; i < 9; i++) {
-      rows[i] = new HashMap<Integer, Integer>();
-      columns[i] = new HashMap<Integer, Integer>();
-      boxes[i] = new HashMap<Integer, Integer>();
-    }
-
-    // validate a board
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        char num = board[i][j];
-        if (num != '.') {
-          int n = (int)num;
-          int box_index = (i / 3 ) * 3 + j / 3;
-
-          // keep the current cell value
-          //getOrDefault(Object key, V defaultValue)当Map集合中有这个key时，就使用这个key值，如果没有就使用默认值defaultValue
-          rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
-          columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
-          boxes[box_index].put(n, boxes[box_index].getOrDefault(n, 0) + 1);
-
-          // check if this value has been already seen before
-          if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || boxes[box_index].get(n) > 1)
-            return false;
-        }
-      }
-    }
-
-    return true;
-  }
-}
-
-
-```
->双向BFS
->①同时从两端进行扩散  ②保证从小的先扩散，当end>start是 置换 ③对比单行BFS有start和end两个Set
-
-[单词接龙](https://leetcode-cn.com/problems/word-ladder/)
-```java
 class Solution {
     Map<String, Integer> wordId = new HashMap<String, Integer>();
     List<List<Integer>> edge = new ArrayList<List<Integer>>();
@@ -146,6 +82,3 @@ class Solution {
         }
     }
 }
-
-
-```
